9d6e894689975294471773852ef24378c7703fae
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-09-03 18:56:20 +0100

    database client and drizzle client created

--- src/db/client.js
+++ src/db/client.js
@@ -1,15 +1,27 @@
-import { neon, neonConfig } from "@neondatabase/serverless";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 import { getDatabaseUrl } from "../lib/secrets.js";
 
 export async function getDbClient() {
   try {
     const dburl = await getDatabaseUrl();
-
-    neonConfig.fetchConnectionCache = true;
     const sql = neon(dburl);
+    
     return sql;
   } catch (error) {
     console.error("Error fetching parameter:", error);
-    throw new Error("Failed to retrieve database URL");
+    throw new Error("Failed to create Neon SQL client");
+  }
+}
+
+export async function getDrizzleDbClient() {
+  try {
+    const client = await getDbClient();
+    const drizzleClient = drizzle(client);
+
+    return drizzleClient;
+  } catch (error) {
+    console.error("Error initalizing Drizzle ORM:", error);
+    throw new Error("Failed to initialize Drizzle ORM");
   }
 }
