87582c989bb457e665d3059f4be7e2b75fef2eb2
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-09-11 05:49:21 +0100

    table schema using neon pg

--- src/db/schemas.js
+++ src/db/schemas.js
@@ -1,9 +1,8 @@
-import { text, pgTable, serial } from "drizzle-orm/pg-core";
+import { text, pgTable, serial, timestamp } from "drizzle-orm/pg-core";
 
 export const LeadTable = pgTable("leads", {
   id: serial("id").primaryKey().notNull(),
   email: text("email").notNull(),
-  username: text("username").notNull(),
-  firstName: text("first_name").notNull(),
-  lastName: text("last_name"),
+  bio: text("bio").default("hanoled user"),
+  createdAt: timestamp("created_at").defaultNow(),
 });
