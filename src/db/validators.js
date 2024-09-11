36c3468ec756abf2a3313fcc4eb27a58c06b1341
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-09-11 05:47:41 +0100

    validation using zod

--- src/db/validators.js
+++ src/db/validators.js
@@ -1,4 +1,7 @@
 import { z } from "zod";
+import { eq } from "drizzle-orm";
+import { getDrizzleDbClient } from "./client.js";
+import * as schemas from "./schemas.js";
 
 export async function validateLeads(postData) {
   const lead = z.object({
@@ -14,13 +17,39 @@ export async function validateLeads(postData) {
     hasError = false;
     message = "";
   } catch (error) {
-    hasError = true;
-    message = "Invalid email. Try again.";
+    return {
+      data: validData,
+      hasError: true,
+      message: "Invalid email. Try again.",
+    };
+  }
+
+  try {
+    const db = await getDrizzleDbClient();
+    const emailData = validData["email"];
+    const existingData = await db
+      .select()
+      .from(schemas.LeadTable)
+      .where(eq(schemas.LeadTable.email, emailData));
+
+    if (existingData.length > 0) {
+      return {
+        data: validData,
+        hasError: true,
+        message: "Email has been registered. Please use a different email.",
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return {
+      hasError: true,
+      message: "An error occured. Please try again.",
+    };
   }
 
   return {
     data: validData,
-    hasError: hasError,
-    message: message,
+    hasError: false,
+    message: "Email valid",
   };
 }
