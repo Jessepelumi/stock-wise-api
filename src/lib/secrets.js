4897e6167375c49365227d3faf603e403808479f
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2024-09-11 04:57:53 +0100

    created putDatabaseUrl function to automate adding database secrets to AWS

--- src/lib/secrets.js
+++ src/lib/secrets.js
@@ -1,4 +1,8 @@
-import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
+import {
+  SSMClient,
+  GetParameterCommand,
+  PutParameterCommand,
+} from "@aws-sdk/client-ssm";
 
 const AWS_REGION = "us-east-2";
 const STAGE = process.env.STAGE || "prod";
@@ -19,6 +23,37 @@ export async function getDatabaseUrl() {
     return paramStoreData.Parameter.Value;
   } catch (error) {
     console.error("Error fetching database URL:", error);
+    throw error;
+  }
+}
+
+export async function putDatabaseUrl(stage = "dev", dbUrlVal) {
+  const paramStage = stage.toLowerCase();
+  if (paramStage === "prod") {
+    console.warn("Cannot update 'prod' environment's database URL");
+    return;
+  }
+  if (!dbUrlVal) {
+    console.warn("No database URL value provided");
+    return;
+  }
+
+  try {
+    const DATABASE_URL_SSM_PARAM = `/stock-wise-api/${paramStage}/database-url`;
+
+    const command = new PutParameterCommand({
+      Name: DATABASE_URL_SSM_PARAM,
+      Value: dbUrlVal,
+      Type: "SecureString",
+      Overwrite: true,
+    });
+
+    const response = await ssmClient.send(command);
+
+    console.log(`Database URL updated for stage: ${paramStage}`);
+    return response;
+  } catch (error) {
+    console.error("Error updating database URL:", error);
     throw error;
   }
 }
